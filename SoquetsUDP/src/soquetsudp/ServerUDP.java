ee53469bcbf44488093a84588213367350407615
Carlos Montellano <carlosmontellano@gmail.com>
2021-09-30 16:22:44 -0400

    30-09-21

---
+++ SoquetsUDP/src/soquetsudp/ServerUDP.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soquetsudp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ServerUDP {
+
+  public static void main (String args[]) { 
+    int port=6789;  
+    try {
+      
+      DatagramSocket socketUDP = new DatagramSocket(port);
+      byte[] bufer = new byte[1000];
+
+      while (true) {
+        // Construimos el DatagramPacket para recibir peticiones
+        DatagramPacket peticion =
+          new DatagramPacket(bufer, bufer.length);
+
+        // Leemos una petición del DatagramSocket
+        socketUDP.receive(peticion);
+
+        System.out.print("Datagrama recibido del host: " +
+                           peticion.getAddress());
+        System.out.println(" desde el puerto remoto: " +
+                           peticion.getPort());
+        
+        
+        // Construimos el DatagramPacket para enviar la respuesta
+        DatagramPacket respuesta =
+          new DatagramPacket(peticion.getData(), peticion.getLength(),
+                             peticion.getAddress(), peticion.getPort());
+
+        // Enviamos la respuesta, que es un eco
+        socketUDP.send(respuesta);
+      }
+
+    } catch (SocketException e) {
+      System.out.println("Socket: " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("IO: " + e.getMessage());
+    }
+  }
+
+}
+
+    
+
